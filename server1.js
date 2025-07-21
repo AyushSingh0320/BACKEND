fd9d6cd143380ba5e1b91a0d5d296edfd9cbac74
Ayush singh <aayushssingh2003@gmail.com>
2025-07-21 18:31:46 +0530

    server is ready

--- server1.js
+++ server1.js
@@ -1,12 +1,59 @@
 const http = require('http');
 const fs = require('fs');   
 const path = require('path');
-console.log("Server is starting...");
+
 
 const port = 3000 ;
 
-const server = http.createServer()
+const server = http.createServer((req , res) => {
+    const filepath = path.join(__dirname, req.url === "/" ? "index.html" : req.url);
+    const extname = String(path.extname(filepath)).toLowerCase();
+    let contentType = "application/octet-stream";
 
+    // Set content type based on file extension
+    switch (extname) {
+        case ".html":
+            contentType = "text/html";
+            break;
+        case ".js":
+            contentType = "application/javascript";
+            break;
+        case ".css":
+            contentType = "text/css";
+            break;
+        case ".json":
+            contentType = "application/json";
+            break;
+        case ".png":
+            contentType = "image/png";
+            break;
+        case ".jpg":
+        case ".jpeg":
+            contentType = "image/jpeg";
+            break;
+        case ".gif":
+            contentType = "image/gif";
+            break;
+        case ".svg":
+            contentType = "image/svg+xml";
+            break;
+        case ".txt":
+            contentType = "text/plain";
+            break;
+    }
+
+    fs.readFile(filepath, (err, content) => {
+        if (err) {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.end("<h1>404 Not Found Broooooooo</h1>");
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content);
+            console.log(`Served: ${req.url}`);
+        }
+    });
+
+})
 server.listen(port , () => {
     console.log(`Server is running on port ${port}`);
 });
